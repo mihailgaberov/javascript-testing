654ce9178d56f5d3bf9cd2fc32cfc97020c5cf2b
Mihail Gaberov <mihail.gaberov@gmail.com>
2017-10-06 17:49:32 +0200

    Updating the slides.

--- src/async/PromiseCallbacks.js
+++ src/async/PromiseCallbacks.js
@@ -4,6 +4,7 @@
 function callbackHell() {
   const api = new Api()
   let user, friends
+
   api.getUser().then(function (returnedUser) {
     user = returnedUser
     api.getFriends(user.id).then(function (returnedFriends) {

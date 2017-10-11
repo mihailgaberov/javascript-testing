ba6c4a7a0d9ed2b319fc263d6435a429a12c0c38
Mihail Gaberov <mihail.gaberov@gmail.com>
2017-10-11 17:03:45 +0200

    Updated .babelrc to handle async functions.

--- test/async/AsyncFriendsBook.test.js
+++ test/async/AsyncFriendsBook.test.js
@@ -1,12 +1,13 @@
 /**
  * Created by mgab on 01/09/2017.
  */
-import { expect } from 'mocha'
+import expect  from 'mocha'
 import AsyncFriendsBook from '../../src/async/AsyncFriendsBook'
 
 describe('AsyncFriendsBook', () => {
   it('should test callback hell using done()', (done) => {
-
+    const expected = AsyncFriendsBook.callbackHell()
+    expect(expected).to.be.equal({})
     done()
   })
 })

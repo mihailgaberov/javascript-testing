166de8c3ee42e078835ffa0df74fa059d2b2aa64
Mihail Gaberov <mihail.gaberov@gmail.com>
2017-10-12 13:26:29 +0200

    Added test for the callback hell using done().

--- test/async/AsyncFriendsBook.test.js
+++ test/async/AsyncFriendsBook.test.js
@@ -5,7 +5,23 @@ import { expect } from 'chai'
 import AsyncFriendsBook from '../../src/async/AsyncFriendsBook'
 
 describe('AsyncFriendsBook', () => {
-  it('should return user info using callbackHell', () => {
+  it('should return user info using callbackHell via done()', (done) => {
+    AsyncFriendsBook.callbackHell().then((result) => {
+      const expected = {
+        user: { id: 1, name: 'Mihail Gaberov' },
+        friends: [
+          { id: 1, name: 'Mihail Gaberov' },
+          { id: 1, name: 'Mihail Gaberov' },
+          { id: 1, name: 'Mihail Gaberov' }
+        ],
+        pic: 'not a real photo'
+      }
+
+      expect(result).to.deep.equal(expected)
+      done()
+    })
+  })
+  it('should return user info using callbackHell via promises', () => {
     return AsyncFriendsBook.callbackHell().then((result) => {
       const expected = {
         user: { id: 1, name: 'Mihail Gaberov' },
